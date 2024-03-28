01db84e4331e1cc2b8e22be35c36fa1a12230e61
sofiaana23 <anasofia.eyre@gmail.com>
2024-03-27 22:13:10 -0600

    Made changes ins spawn manager. Food and damage spawn correctly.

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,34 +5,40 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 
 {
-    public GameObject[] food;
-    public GameObject enemies;
-
+    public GameObject[] environmentObjects;
     private float zFoodSpawn = 12.0f;
     private float xSpawnRange = 16.0f;
-    private float zEnemiesRange = 5.0f;
     private float ySpawn = 0.75f;
+
+    public float sleepTime = 1f;
+    private float currentSleep = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        SpawnFood();
+        SpawnObjects();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (currentSleep <= 0f){
+            SpawnObjects();
+            currentSleep = Random.Range(0,sleepTime);
+        }
+        else {
+            currentSleep -= Time.deltaTime;
+        }
     }
 
-    void SpawnFood ()
+    void SpawnObjects ()
     {
-        float randomX = randomX.Range(-xSpawnRange, xSpawnRange);
-        int randomIndex = randomIndex.Range(0, food.Lenght);
+        float randomX = Random.Range(-xSpawnRange, xSpawnRange);
+        int randomIndex = Random.Range(0, environmentObjects.Length);
 
         Vector3 spawnPos = new Vector3(randomX, ySpawn, zFoodSpawn);
 
-        Instantiate(food[randomIndex], spawnPos, food[randomIndex].gameObject.transform.rotation);
+        Instantiate(environmentObjects[randomIndex], spawnPos, environmentObjects[randomIndex].gameObject.transform.rotation);
     }
 }
 
